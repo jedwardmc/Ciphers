d7e8ad12bf6b645073e4d2327928d5004c95c75d
Джазман - Jazzman <jasper.cherry@gmail.com>
2019-12-23 09:26:49 +1100

    Update HillCipher.java

--- HillCipher.java
+++ HillCipher.java
@@ -1,99 +1,80 @@
 import java.util.*;
 
-/*
- * Unique implementation of the Hill cipher.
- * - 
- * @author: jedwardmc
- * @date: 9.09.2018
+/* Unique implementation of the Hill Cipher.
+ * Redesigned in 2019.
+ * @author: Jasper Cherry
+ * @date: 23.12.2019
  */
 
 public class HillCipher {
+    private static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    private static Scanner inputScanner = null;
+    private static String inputStr = null;
     
-    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
-    
-    public static void main(String[] args) {
-        
-        int[] keyMatrix = {6, 14, 3, 0};
-        encrypt("love does not boast", keyMatrix);
+    public static String prompt(String msg) {
+        System.out.println(msg);
+        inputScanner = new Scanner(System.in);
+        inputStr = inputScanner.nextLine();
+        return inputStr;
     }
     
-    
-    public static void encrypt(String plainText, int[] keyMatrix) {
+    public static void encrypt(String plainText, String keyMatrix) {
         
-        // Clean up the plain text for later use
+        int[] alphNums = new int[plainText.length()];
+        int xval, yval, dxval, dyval;
+        char rxval, ryval;
         
+        
+        /* clean up the plain text for later use */
         plainText = plainText.toLowerCase();
         plainText = plainText.replaceAll("[^a-zA-Z]+", "");
         
-        // Add an additional character if the length is odd
-        // This will assist in creating a digraph later on
+        if (plainText.length() % 2 != 0) plainText += "e";
         
-        if(plainText.length() % 2 != 0) {
-            plainText += "e";
+        for(int ii=0; ii < plainText.length(); ++ii) {
+            int tempPos = plainText.charAt(ii) - 'a' + 1;
+            alphNums[ii] = tempPos;
         }
         
-        // Shorthand variable for storing the length of plainText
-        int length = plainText.length();
+        String[] tempMatrix = keyMatrix.split(",", 0);
+        int[] matrix = new int[tempMatrix.length + 1];
         
-        // Stores numerical equivalents of each letter
-        int[] numbers = new int[length];
-        
-        // Get the numerical equivalent of all letters in the plaintext
-        
-        for (int i=0; i < length; ++i) {
-            
-            // Get the numerical equivalent of each letter in the alphabet
-            int charValue = ALPHABET.indexOf(plainText.charAt(i));
-            
-            // Add them the numbers array list
-            numbers[i] = charValue;
-
+        try {
+            int i=0;
+            for (String s : tempMatrix ) {
+                i++;
+                int newKey = Integer.parseInt(s);
+                matrix[i] = newKey;
+            }
+        } catch(Exception e) {
+            System.out.println("Invalid key matrix." + e);
         }
         
-        // Digraphs need X and Y values in the format (0 1) (2 3) (.. ..)
-        // Declare two integer arrays containing the indices for finding
-        // numbers in the numbers array
         
-        int[] xi = new int[numbers.length/2];
-        int[] yi = new int[numbers.length/2];
+        int[] xi = new int[alphNums.length/2];
+        int[] yi = new int[alphNums.length/2];
         
-        // Generate the ciphertext
-        
-        System.out.println();
-        System.out.println("HILL CIPHER ::::::::::::::");
-        System.out.println("\t KEY: " + Arrays.toString(keyMatrix));
-        System.out.println("\t ORIGINAL TEXT: " + plainText);
-        System.out.print("\t CIPHER TEXT: ");
-        
-      
-        for(int n=0; n < numbers.length / 2; n++) {
-            xi[n] = n * 2;
-            yi[n] = xi[n] + 1;
+        for (int nn=0; nn < alphNums.length / 2; ++nn) {
+            xi[nn]=nn*2;
+            yi[nn]=xi[nn]+1;
             
-            // Get the numbers using the created indices
+            xval = alphNums[xi[nn]];
+            yval = alphNums[yi[nn]];
             
-            int xVal = numbers[ xi[n] ];
-            int yVal = numbers[ yi[n] ];
+            dxval = (xval * matrix[0] + matrix[1] * yval) % 26;
+            dyval = (xval * matrix[2] + matrix[3] * yval) % 26;
             
-            // Generate the digraphs using matrix multiplication
+            rxval = alphabet.charAt(dxval);
+            ryval = alphabet.charAt(dyval);
             
-            int digraphX = (xVal * keyMatrix[0] + keyMatrix[1] * yVal) % 26;
-            int digraphY = (xVal * keyMatrix[2] + keyMatrix[3] * yVal) % 26;
-            
-            // Transform them into textual equivalents
-            
-            char resultX = ALPHABET.charAt(digraphX);
-            char resultY = ALPHABET.charAt(digraphY);
-            
-            // Print the final result in digraph form
-            
-            System.out.print("(" + resultX + " " + resultY + ")");
-            
-
+            System.out.print("(" + rxval + " " + ryval + ")");
         }
-
-        // Print a new line 
-        System.out.println("\n");
-
+        System.out.println(); 
+    }
+    
+    public static void main(String[] args) {
+        String plainTextStr = prompt("Enter a plain text: ");
+        String keyMatrixStr = prompt("Enter a key matrix: ");
+        encrypt(plainTextStr, keyMatrixStr);
     }
 }
